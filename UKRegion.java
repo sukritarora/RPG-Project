b9ad8531cf7130359f78aeead9ca48a5ee0a02e8
sukritarora <sukrit.arora2211@gmail.com>
2015-02-21 19:13:00 -0800

    Stuff done

--- UKRegion.java
+++ UKRegion.java
@@ -8,7 +8,7 @@
 public class UKRegion extends Regions
 {
     // instance variables - replace the example below with your own
-    private int x;
+    
 
     /**
      * Constructor for objects of class UKRegion
@@ -17,18 +17,9 @@ public class UKRegion extends Regions
     {
         // initialise instance variables
         super("United Kingdom");
-        x = 0;
+        
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
